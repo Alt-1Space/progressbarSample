70af288dc6ae8499f9ab4de060d65ecea19274d4
aster <aster@KEI>
2024-03-25 23:17:16 +0900

    プロジェクト ファイルを追加します。

---
+++ progressbarSample/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace progressbarSample
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            // ProgressBarの設定
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 30;
+            progressBar1.Visible = false;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            using (var progressForm = new ProgressForm())
+            {
+                progressForm.Show();
+
+                // 非同期処理の開始
+                await Task.Run(() => DoWork());
+
+                // 処理が完了したらポップアップを閉じる
+                progressForm.Close();
+            }
+
+            //// ProgressBarを表示
+            //progressBar1.Visible = true;
+
+            //// 何らかの非同期処理を開始
+            //await Task.Run(() => DoWork());
+
+            //// ProgressBarを非表示
+            //progressBar1.Visible = false;
+        }
+
+        // 仮の非同期で実行する長い処理
+        private void DoWork()
+        {
+            // ここに長い処理を書く
+            System.Threading.Thread.Sleep(5000); // 仮に5秒間のスリープを入れる
+        }
+    }
+}
---
+++ progressbarSample/ProgressForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progressbarSample
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressBar progressBar;
+        public Label label;
+
+        public ProgressForm()
+        {
+            progressBar = new ProgressBar();
+            label = new Label();
+
+            // ProgressBarの設定
+            progressBar.Style = ProgressBarStyle.Marquee;
+            progressBar.MarqueeAnimationSpeed = 30;
+            progressBar.Dock = DockStyle.Bottom;
+
+            // Labelの設定
+            label.Text = "処理を実行しています...";
+            label.Dock = DockStyle.Top;
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // ProgressFormの設定
+            this.Controls.Add(progressBar);
+            this.Controls.Add(label);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new System.Drawing.Size(300, 100);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowInTaskbar = false;
+        }
+    }
+}
